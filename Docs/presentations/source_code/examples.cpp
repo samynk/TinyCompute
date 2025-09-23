cb093592af0573a817c28cbe136c07bc26e862cb
Koen Samyn <samyn.koen@gmail.com>
2025-09-23 14:17:49 +0200

    Removed files.

--- Docs/presentations/source_code/examples.cpp
+++
@@ -1,9 +0,0 @@
-bool VisitCXXNewExpr(clang::CXXNewExpr* E){
-	llvm::errs() 
-    << E->getBeginLoc().printToString(Context.getSourceManager()) 
-    << " Error: 'new' is not allowed in kernel, only value types are allowed.\n";
-    
-	reportError(E->getBeginLoc(), dynamicMemory);
-	Valid = false;
-	return true;
-}
